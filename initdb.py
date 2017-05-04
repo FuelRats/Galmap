c306e426144a81737de1016a309cafafe9b7ae6a
Kenneth Aalberg <kenneaal@gmail.com>
2017-05-04 20:21:23 +0200

    Use proper .path.getmtime... Yeesh.
    Signed-off-by: Kenneth Aalberg <kenneaal@gmail.com>

--- initdb.py
+++ initdb.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, sys
 from datetime import time
 
 from odo import odo, dshape
@@ -36,7 +35,7 @@ def main(argv=sys.argv):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     # Add all the systems!
-    if sys.path.getmtime('systems.csv') > time.time()-(24*3600):
+    if os.path.getmtime('systems.csv') > time.time()-(24*3600):
         print("Using cached systems.csv")
     else:
         print("Downloading systems.csv from EDDB.io...")
@@ -62,7 +61,7 @@ def main(argv=sys.argv):
     print("Creating indexes...")
     DBSession.execute("CREATE INDEX systems_idx on systems(name)")
     print("Done!")
-    if sys.path.getmtime('bodies.jsonl') > time.time() - (24 * 3600):
+    if os.path.getmtime('bodies.jsonl') > time.time() - (24 * 3600):
         print("Using cached bodies.jsonl")
     else:
         print("Downloading bodies.jsonl from EDDB.io...")
