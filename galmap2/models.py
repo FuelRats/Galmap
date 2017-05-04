d52bacfee3e461e07b711139f6943cbd249e5c40
Kenneth Aalberg <kenneaal@gmail.com>
2017-05-04 21:53:12 +0200

    MORE integer size???!
    Signed-off-by: Kenneth Aalberg <kenneaal@gmail.com>

--- galmap2/models.py
+++ galmap2/models.py
@@ -31,7 +31,7 @@ class Body(Base):
     created_at = Column(BigInteger)
     updated_at = Column(BigInteger)
     name = Column(Text)
-    system_id = Column(Integer)
+    system_id = Column(BigInteger)
     group_id = Column(Integer)
     group_name = Column(Text)
     type_id = Column(Integer)
