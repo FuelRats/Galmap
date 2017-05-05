a01cdd8342e717b3f8be252194ee17dc629987de
Kenneth Aalberg <kenneaal@gmail.com>
2017-05-05 16:14:22 +0200

    Move ini files to .default postfix to provide better git pullage.
    Signed-off-by: Kenneth Aalberg <kenneaal@gmail.com>

--- initdb.py
+++ initdb.py
@@ -20,7 +20,7 @@ from galmap2.models import (
 def usage(argv):
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri>\n'
-          '(example: "%s development.ini")' % (cmd, cmd))
+          '(example: "%s development.ini.default")' % (cmd, cmd))
     sys.exit(1)
 
 
