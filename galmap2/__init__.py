d69122b496bc8facdf021b7becb1e52b6e2bf2a2
Kenneth Aalberg <kenneaal@gmail.com>
2017-05-07 22:07:27 +0200

    Try to apply magic caching to API queries using pyramid_beaker part 2.
    Signed-off-by: Kenneth Aalberg <kenneaal@gmail.com>

--- galmap2/__init__.py
+++ galmap2/__init__.py
@@ -7,15 +7,21 @@ from . import models
 import pyramid_jsonapi
 
 import pyramid_beaker
+from pyramid_beaker import set_cache_regions_from_settings
+from pyramid_beaker import session_factory_from_settings
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    set_cache_regions_from_settings(settings)
+    session_factory = session_factory_from_settings(settings)
+
     engine = engine_from_config(settings, 'sqlalchemy.')
     models.DBSession.configure(bind=engine)
     models.Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+    config.set_session_factory(session_factory)
     config.include('pyramid_beaker')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_static_view(name='textures', path='static/ED3D-Galaxy-Map/textures')
